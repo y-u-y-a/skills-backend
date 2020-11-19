09611676367350b66f0885e2434ff6c6aae35bd5
y-u-y-a <yuya.ymd00@gmail.com>
2020-11-19 19:00:47 +0900

    PageNav作成

---
+++ frontend/code/src/components/templates/PageNav.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="form-nav">
+    <div class="contents row align-items-center justify-content-between">
+      <ul class="row">
+        <li v-for="n in 3" :key="n">
+          <CircleItem :text="n" />
+        </li>
+      </ul>
+      <Button button_name="ダウンロード" class="px-1" />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.form-nav {
+  padding: 0.5rem 0;
+  background: $light-blue;
+}
+ul {
+  max-width: 720px;
+  width: 100%;
+  li {
+    margin-right: 2rem;
+  }
+}
+</style>
