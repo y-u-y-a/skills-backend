088478f05dce89743fcfa81592adfe4656073cbe
y-u-y-a <yuya.ymd00@gmail.com>
2020-11-21 01:29:42 +0900

    Footerの固定方法変更

--- frontend/code/src/components/organisms/TheFooter.vue
+++ frontend/code/src/components/organisms/TheFooter.vue
@@ -1,40 +1,42 @@
 <template>
   <footer>
-    <ul class="row justify-content-between">
-      <li>
-        <a href="#">利用規約</a>
-      </li>
-      <li>
-        <a href="#">プライバシーポリシー</a>
-      </li>
-      <li>
-        <a href="#">お問い合わせ</a>
-      </li>
-    </ul>
-    <div class="copyright">
-      <small>Skills ©︎ 2020</small>
+    <div class="footer-wrapper contents">
+      <ul class="row justify-content-between">
+        <li>
+          <a href="#">利用規約</a>
+        </li>
+        <li>
+          <a href="#">プライバシーポリシー</a>
+        </li>
+        <li>
+          <a href="#">お問い合わせ</a>
+        </li>
+      </ul>
+      <div class="copy-right">
+        <small>Skills ©︎ 2020</small>
+      </div>
     </div>
   </footer>
 </template>
 
 <style lang="scss" scoped>
 footer {
-  position: fixed;
-  bottom: 0;
-  width: 100%;
-  padding: 0.5rem 0;
+  margin-top: auto; // Fixed Footer
+  background-color: $dark-brown;
+  .footer-wrapper {
+    @include row(between, center);
+  }
   ul {
-    max-width: 350px;
-    margin: 0 auto 0.5rem;
+    padding: 1rem 0;
+    a {
+      margin-right: 1rem;
+      font-size: 14px;
+      color: $white;
+    }
   }
-  a {
-    font-size: 14px;
-    color: $white;
-  }
-  .copyright {
+  .copy-right {
     font-size: 12px;
     color: $white;
-    text-align: center;
   }
 }
 </style>
--- frontend/code/src/layouts/default.vue
+++ frontend/code/src/layouts/default.vue
@@ -5,3 +5,12 @@
     <TheFooter />
   </div>
 </template>
+
+<style lang="scss" scoped>
+// Fixed Footer
+.container {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+</style>
