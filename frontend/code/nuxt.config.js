1dc5f7da3dffe9f49348207fe031b01c6575ad25
y-u-y-a <yuya.ymd00@gmail.com>
2020-11-22 17:47:35 +0900

    lang jaに変更

--- frontend/code/nuxt.config.js
+++ frontend/code/nuxt.config.js
@@ -2,6 +2,7 @@ export default {
   srcDir: 'src/',
   head: {
     title: 'Skills',
+    htmlAttrs:{ lang: 'ja'},
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
