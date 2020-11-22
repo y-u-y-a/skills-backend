44a545894a676f752b29b14f3c5e32352249e089
y-u-y-a <yuya.ymd00@gmail.com>
2020-11-22 17:53:25 +0900

    index.vue作成

--- frontend/code/src/pages/index.vue
+++ frontend/code/src/pages/index.vue
@@ -1,43 +1,57 @@
 <template>
-  <div class="column justify-content-around align-items-center">
-    <!-- store/index -->
-    <div class="contents mb-2">
-      <button @click="countPlus()">増加</button>
-      <button @click="countMinus()">減少</button>
+  <div class="contents column justify-content-around align-items-center">
+    <div class="title">
+      <h2>
+        ブラウザ上で簡単に<br class="sp">
+        スキルシートが作れます！
+      </h2>
+      <p>
+        Skillsは、ブラウザ上でスキルシート作成ができるサービスです。<br>
+        作成後は、PDFダウンロード、共有リンク発行ができます。
+      </p>
     </div>
-    <div>{{ count }}</div>
-    <!-- store/qiita -->
-    <ul>
-      <li v-for="(article, index) in articles" :key="index">
-        <a :href="article.url" target="_blank">
-          {{ index + 1 }}：{{ article.title }}
-          </a>
-      </li>
-    </ul>
+    <NuxtLink to="/user/profile">
+      プロフィール入力を始める
+    </NuxtLink>
+    <NuxtLink to="/project/About">
+      案件の入力を始める
+    </NuxtLink>
+    <div class="row">
+      <TextButton button_name="使い方を見る" />
+    </div>
   </div>
 </template>
 
-<script>
-import { mapGetters, mapActions } from 'vuex';
-
-export default {
-  computed: {
-    ...mapGetters({
-      count: 'counter',
-      articles: 'qiita/articles'
-    }),
-  },
-
-  created() {
-    this.getArticles();
-  },
-
-  methods: {
-    ...mapActions({
-      countPlus: 'plus',
-      countMinus: 'minus',
-      getArticles: 'qiita/getArticles'
-    }),
+<style lang="scss" scoped>
+.title {
+  margin: 4rem 0;
+  text-align: center;
+  h2 {
+    margin-bottom: 3rem;
+    font-size: 28px;
   }
-};
-</script>
+  p {
+    margin-bottom: 1rem;
+    line-height: 32px;
+  }
+}
+a {
+  @include fontColor;
+  width: 20rem;
+  padding: 1rem;
+  margin-bottom: 2rem;
+  background: $white;
+  border: 1px solid $gray;
+  border-radius: 5px;
+  font-size: 18px;
+  text-align: center;
+}
+a:hover {
+  @include focusStyle;
+}
+@media screen and (max-width: $bp) {
+  h2 {
+    line-height: 42px;
+  }
+}
+</style>
