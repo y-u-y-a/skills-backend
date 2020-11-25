b11331caac052eaf8790cfee6ee569de521a92d4
y-u-y-a <yuya.ymd00@gmail.com>
2020-11-25 17:15:23 +0900

    Tag.vue作成

---
+++ frontend/code/src/components/atoms/Tag.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="tag d-inline-block">
+    {{ value }}
+    <Icon @click="emitter" icon="times" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['value'],
+  methods: {
+    emitter(e) {
+      this.$emit('click', this.value);
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.tag {
+  padding: 0.3rem 0.5rem;
+  background: $light-gray;
+  border-radius: 20px;
+  font-size: 14px;
+  cursor: default;
+  svg:hover {
+    opacity: 0.5;
+    transition: 0.1s;
+    cursor: pointer;
+  }
+}
+</style>
