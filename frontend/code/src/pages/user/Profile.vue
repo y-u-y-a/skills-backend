0823bb754f8107e76ac22548cca7480c0624a17a
y-u-y-a <yuya.ymd00@gmail.com>
2020-11-26 16:44:02 +0900

    kana追加

--- frontend/code/src/pages/user/Profile.vue
+++ frontend/code/src/pages/user/Profile.vue
@@ -1,17 +1,29 @@
 <template>
   <Form form_title="プロフィールを入力" class="mb-2">
     <template v-slot:content>
-      <div class="row">
+      <div class="row justify-content-between">
         <InputGroup
           v-model="profile.last_name"
           label="姓"
           placeholder="鈴木"
-          class="w-30 mr-2" />
+          class="w-48" />
+        <InputGroup
+          v-model="profile.last_name_kana"
+          label="セイ"
+          placeholder="スズキ"
+          class="w-48" />
+      </div>
+      <div class="row justify-content-between">
         <InputGroup
           v-model="profile.first_name"
           label="名"
           placeholder="太郎"
-          class="w-30" />
+          class="w-48" />
+        <InputGroup
+          v-model="profile.first_name_kana"
+          label="メイ"
+          placeholder="タロウ"
+          class="w-48" />
       </div>
       <div class="row flex-wrap">
         <SelectGroup
@@ -19,20 +31,20 @@
           label="性別"
           :val_list="['未入力', '男性', '女性']"
           class="mr-4" />
-          <div class="row align-items-end">
-            <SelectGroup
-              v-model="profile.birth.year"
-              label="生年月日"
-              :val_list="['1990']"
-              class="mr-1" />
-            <SelectGroup
-              v-model="profile.birth.month"
-              :val_list="['04']"
-              class="mr-1" />
-            <SelectGroup
-              v-model="profile.birth.day"
-              :val_list="['10']" />
-          </div>
+        <div class="row align-items-end">
+          <SelectGroup
+            v-model="profile.birth.year"
+            label="生年月日"
+            :val_list="['1990']"
+            class="mr-1" />
+          <SelectGroup
+            v-model="profile.birth.month"
+            :val_list="['04']"
+            class="mr-1" />
+          <SelectGroup
+            v-model="profile.birth.day"
+            :val_list="['10']" />
+        </div>
       </div>
       <InputGroup
         v-model="profile.email"
@@ -46,12 +58,8 @@
         v-model="profile.station"
         label="最寄駅"
         placeholder="例：丸ノ内線/東京駅" />
-      <InputGroup
-        v-model="profile.share_link"
-        label="共有リンク"
-        placeholder="例：https://github.com/xxxx" />
       <TextGroup
-        v-model="profile.pr"
+        v-model="profile.pr_text"
         label="PR"
         placeholder="自己PRを入力してください。" />
     </template>
@@ -74,6 +82,8 @@ export default {
       profile: {
         last_name: '鈴木',
         first_name: '一郎',
+        last_name_kana: 'スズキ',
+        first_name_kana: 'タロウ',
         gender: '男性',
         birth: {
           year: '1990',
@@ -83,8 +93,7 @@ export default {
         email: 'hoge@gmail.com',
         phone_number: '000-1111-3333',
         station: '丸ノ内線/竹橋駅',
-        share_link: null,
-        pr: '自己PRの文章。自己PRの文章。自己PRの文章。自己PRの文章。自己PRの文章。'
+        pr_text: '自己PRの文章。自己PRの文章。自己PRの文章。自己PRの文章。自己PRの文章。'
       }
     }
   },
--- frontend/code/src/store/user.js
+++ frontend/code/src/store/user.js
@@ -1,6 +1,22 @@
 import axios from 'axios';
 
 export const state = () => ({
+  profile: {
+    last_name: null,
+    first_name: null,
+    last_name_kana: null,
+    first_name_kana: null,
+    gender: null,
+    birth: {
+      year: null,
+      month: null,
+      day: null
+    },
+    email: null,
+    phone_number: null,
+    station: null,
+    pr_text: null
+  }
 });
 
 export const getters = {
