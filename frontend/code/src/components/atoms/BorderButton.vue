c9785e6e8d86dc016556d5bad7c1590511b0c7fa
y-u-y-a <yuya.ymd00@gmail.com>
2020-11-26 20:01:02 +0900

    BorderButtonとBorderLinkを作成

--- frontend/code/src/components/atoms/BorderButton.vue
+++ frontend/code/src/components/atoms/BorderButton.vue
@@ -1,9 +1,16 @@
 <template>
   <button @click="$emit('trigger')" type="button">
-    <Icon v-if="before_icon" :icon="before_icon" />
-    {{ button_name }}
-    <Icon v-if="after_icon" :icon="after_icon" />
-    </button>
+    <!-- PC -->
+    <span class="pc">
+      <Icon v-if="before_icon" :icon="before_icon" />
+        {{ button_name }}
+        <Icon v-if="after_icon" :icon="after_icon" />
+    </span>
+    <!-- SP -->
+    <span class="sp">
+      <Icon v-if="sp_icon" :icon="sp_icon" />
+    </span>
+  </button>
 </template>
 
 <script>
@@ -11,7 +18,8 @@ export default {
   props: [
     'button_name',
     'before_icon',
-    'after_icon'
+    'after_icon',
+    'sp_icon',
   ]
 }
 </script>
@@ -21,14 +29,19 @@ button {
   @include fontColor;
   width: 8rem;
   padding: 0.5rem 0;
+  background-color: $white;
   border: 1px solid $gray;
   border-radius: 5px;
   text-align: center;
-  background-color: $white;
   cursor: pointer;
 }
 button:hover {
   @include focusStyle;
   transition: 0.2s;
+}
+@include media(sm) {
+  button {
+    width: 3rem;
+  }
 }
 </style>
