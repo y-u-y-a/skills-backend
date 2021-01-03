ee596ad72bda55a04bcc593862a3e48690cb2bce
y-u-y-a <yuya.ymd00@gmail.com>
2021-01-03 22:17:48 +0900

    yarnに変更

--- code/src/graphql/querys/taskQuery.ts
+++ code/src/graphql/querys/taskQuery.ts
@@ -1,12 +1,12 @@
-// import fakeDatabase from '@/config/fakeDatabase'
 import Task from '@/entity/task'
 
 export default {
+  // 一覧取得
   tasks: async (_: any) => {
     const allTasks = await Task.find()
     return allTasks
-    // return fakeDatabase
   },
+  // 1件取得
   task: async (_: any, req: any) => {
     return 'task'
   },
