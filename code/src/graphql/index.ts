fc052a79ee61b6b501a1037886fa06517094ec2a
y-u-y-a <yuya.ymd00@gmail.com>
2021-01-03 15:29:26 +0900

    修正

--- code/src/graphql/index.ts
+++ code/src/graphql/index.ts
@@ -1,15 +1,17 @@
 import path from 'path'
 import { importSchema } from 'graphql-import'
 import { makeExecutableSchema } from 'graphql-tools'
+
+// Import schema files
+const schemaPath = path.resolve(__dirname, './schema/index.gql')
+const typeDefs = importSchema(schemaPath)
+
 // Query
 import userQuery from '@querys/userQuery'
 import taskQuery from '@querys/taskQuery'
 import todoQuery from '@querys/todoQuery'
 // Mutation
 import userMutation from '@mutations/userMutation'
-
-const absolutePath = path.resolve(__dirname, './schema.gql')
-const typeDefs = importSchema(absolutePath)
 
 const resolvers = {
   Query: Object.assign(userQuery, taskQuery, todoQuery),
--- code/src/graphql/mutations/userMutation.ts
+++ code/src/graphql/mutations/userMutation.ts
@@ -4,4 +4,10 @@ export default {
   createUser: async (_: any, req: any) => {
     return req
   },
+  updateUser: async (_: any, req: any) => {
+    return req
+  },
+  deleteUser: async (_: any, req: any) => {
+    return req
+  },
 }
