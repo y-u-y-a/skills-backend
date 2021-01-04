6f099313cc17f0867071944006c5ac9b5d608ecb
y-u-y-a <yuya.ymd00@gmail.com>
2021-01-04 11:13:41 +0900

    修正

--- code/src/graphql/index.ts
+++ code/src/graphql/index.ts
@@ -1,17 +1,16 @@
 import path from 'path'
 import { importSchema } from 'graphql-import'
 import { makeExecutableSchema } from 'graphql-tools'
-
-// Import schema files
-const schemaPath = path.resolve(__dirname, './schema/index.gql')
-const typeDefs = importSchema(schemaPath)
-
 // Query
 import userQuery from '@querys/userQuery'
 import taskQuery from '@querys/taskQuery'
 // Mutation
 import userMutation from '@mutations/userMutation'
 import taskMutation from '@mutations/taskMutation'
+
+// Import schema files
+const schemaPath = path.resolve(__dirname, './schema/index.gql')
+const typeDefs = importSchema(schemaPath)
 
 const resolvers = {
   Query: Object.assign(userQuery, taskQuery),
