093a9745793c22d1cb0e6ff0c35626f7d8104c68
y-u-y-a <yuya.ymd00@gmail.com>
2021-01-15 00:04:49 +0900

    ディレクトリ修正

---
+++ mysql/init/users.sql
@@ -0,0 +1,11 @@
+-- CREATE TABLE users (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     email VARCHAR(255) NOT NULL,
+--     password VARCHAR(255) NOT NULL,
+--     created DATETIME,
+--     modified DATETIME
+-- );
+
+-- INSERT INTO users (email, password, created, modified)
+-- VALUES
+-- ('test@gmail.com', 'secret', NOW(), NOW());
